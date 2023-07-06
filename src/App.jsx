cf257bec0e4721635833d1ab772530a29289d6a5
janetpaul00 <84874007+janetpaul00@users.noreply.github.com>
2023-07-06 11:52:22 +0500

    setting up the main layout

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div id="container">
+      <h1>Rept</h1>
+    </div>
+  );
+}
+
+export default App;
